4d7c0e124186f3813e1060f1f88bffb1cf1e5642
wangjue <wangjue60@yahoo.com>
2021-01-19 00:21:50 -0500

    REST/HiddenHttpMethodFilter

--- springMVC/src/com/springmvc/handlers/SpringMVCTest.java
+++ springMVC/src/com/springmvc/handlers/SpringMVCTest.java
@@ -10,7 +10,24 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class SpringMVCTest {
 	private static final String SUCCESS="success";
-	
+	/**
+	 * Rest 风格的URL
+	 * 以CRUD为例：					以前不用rest风格
+	 * 新增： /order POST				
+	 * 修改:	 /order/1 PUT			update?id=1
+	 * 获取： /order/1 GET			get?id=1
+	 * 删除： /order/1 DELETE			delete?id=1
+	 * 
+	 * 如何发送PUT请求和DELETE请求？
+	 * 	1、需要配置HiddenHttpMethodFilter
+	 *  2、需要发送POST请求
+	 *  3、需要在发送POST请求时携带一个name="_method"的隐藏域，值为DELETE或PUT
+	 *  
+	 * 在SpringMVC的目标方法中如何得到id呢？
+	 * 	--使用@PathVariable 注解
+	 * @param id
+	 * @return
+	 */
 	@RequestMapping(value="/testRest/{id}", method=RequestMethod.DELETE)
 	@ResponseBody()
 	public String testRestDelete(@PathVariable("id")Integer id) {
